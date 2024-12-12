9ff3d6fadca91d3de1b003d685d408eba27b321c
joystanistreet <126020147+joystanistreet@users.noreply.github.com>
2024-12-12 09:23:19 -0400

    Update analysis_status.R

--- analysis_status.R
+++ analysis_status.R
@@ -17,150 +17,135 @@
 
 pacman::p_load(tidyverse,
                here,
-               ggpattern)
-
-#--------------------------------------
-
-# Set options
-
-# projects to leave out
-omit_project <- c('OPP-MEQ Coastal Monitoring', 'Kenchington PAM Landers')
-
-# stations to leave out
-omit_station <- c('ROBE', 'SABV', 'SBSM', 'CGL', 'EGL')
-
-# deployments to leave out
-omit_deployment <- c('CBN-2022-10', 'CSW-2022-10')
-
-# years to leave out
-omit_year <- c('2023', '2024')
+               readxl)
 
 #--------------------------------------
 
 # Compile recording effort summary
 
 # path to PAM metadata folder
-metadata <- 'R:/Science/CetaceanOPPNoise/CetaceanOPPNoise_2/PAM_metadata'
+metadata_folder <- 'R:/Science/CetaceanOPPNoise/CetaceanOPPNoise_2/PAM_metadata'
 
 # load deployment summary
-depl <- read_csv(here(metadata, 'deployment_summary.csv'))
+depl_metadata <- read_csv(here(metadata_folder, 'deployment_summary.csv')) %>% 
 
-target_deployments <- depl %>% 
-  
-  # omit non-target projects
-  filter(!Project %in% omit_project) %>% 
-  
-  # omit non-target deployments
-  mutate(station = stringr::str_extract(Station, '(.)+(?=:)')) %>%
-  filter(!station %in% omit_station) %>% 
-  filter(!Deployment %in% omit_deployment) %>% 
-  
-  # omit current year(s)
-  filter(!Year %in% omit_year) %>% 
-  
-  # remove datasets with no data
+  mutate(deployment = Deployment) %>% 
   mutate(rec_start = as_date(`In-water_start`),
-          rec_end = as_date(`In-water_end`)) %>%
+          rec_end = as_date(`In-water_end`))
+
+# read in analysis status xlsx
+analysis_status <- read_excel(here(metadata_folder, 'analysis_status', 'analysis_status.xlsx'))
+
+#join with metadata
+analysis_status_metadata<-analysis_status %>% 
+  
+  left_join(depl_metadata, by = 'deployment') %>% 
+  
+  # remove deployments with no valid dataset
   filter(!(is.na(rec_start))) %>% 
   
-  # add status columns
-  mutate(narw_status = NA,
-         baleen_status = NA,
-         beaked_status = NA)
-
-# SAVE
-write_csv(target_deployments, 'PAM_status.csv')
-
-### ADD STATUS HERE
-
-#read in manually created PAM status sheet
-analysis_status<-read_csv("PAM_status.csv") %>% 
+  # convert to real NAs in analysis status columns
+  mutate(analysis_narw = as.numeric(analysis_narw),
+         analysis_baleen = as.numeric(analysis_baleen),
+         analysis_beaked = as.numeric(analysis_beaked)) %>% 
   
-  # calculate recording days per deployment
-  mutate(rec_days = as.numeric(as_date(rec_end) - as_date(rec_start) + 1)) %>%
+  # calculate baleen recording days per deployment
+  mutate(rec_days_baleen = as.numeric(as_date(rec_end) - as_date(rec_start) + 1)*record_baleen) %>%
+  
+  # calculate beaked recording days per deployment
+  mutate(rec_days_beaked = as.numeric(as_date(rec_end) - as_date(rec_start) + 1)*record_beaked) %>%
   
   # narw analysis days
-  mutate(narw_days = narw_status*rec_days) %>% 
+  mutate(narw_days = analysis_narw*rec_days_baleen) %>% 
   
   # baleen whale analysis days
-  mutate(baleen_days = baleen_status*rec_days) %>% 
+  mutate(baleen_days = analysis_baleen*rec_days_baleen) %>% 
   
-  # calculate beaked whale recording days per deployment
-  mutate(rec_days_beaked = case_when(beaked_status == 1 | beaked_status ==0 ~ rec_days,
-                                     .default = 0)) %>% 
   # beaked whale analysis days
-  mutate(beaked_days = beaked_status*rec_days_beaked)
+  mutate(beaked_days = analysis_beaked*rec_days_beaked)
   
 # summarize by year
-status_summary<- analysis_status %>% 
+status_summary<- analysis_status_metadata %>% 
   group_by(Year) %>% 
-  summarize(rec_days_year = sum(rec_days),
+  summarize(rec_days_baleen_year = sum(rec_days_baleen),
          rec_days_beaked_year = sum(rec_days_beaked),
-         narw_days_year = sum(narw_days),
-         baleen_days_year = sum(baleen_days),
+         narw_days_year = sum(narw_days, na.rm = T),
+         baleen_days_year = sum(baleen_days, na.rm = T),
          beaked_days_year = sum(beaked_days, na.rm = T)) %>% 
-  mutate(Year = as_factor(Year)) %>% 
+  mutate(year = as_factor(Year)) %>% 
   ungroup()
+
+#--------------------------------------
+
+# Set up data for figure
 
 # baleen whale data: set up for plot
 baleen_summary<-status_summary %>% 
-  transmute(Year, n_days = rec_days_year, analyzed = baleen_days_year) %>% 
-  #select(year, rec_days_year, baleen_days_year) %>% 
+  transmute(year, n_days = rec_days_baleen_year, analyzed = baleen_days_year) %>% 
   mutate(not_analyzed = n_days - analyzed) %>% 
   select(-n_days) %>% 
-  pivot_longer(!Year, names_to = "effort", values_to = "count") %>% 
+  pivot_longer(!year, names_to = "effort", values_to = "count") %>% 
   mutate(effort = factor(effort, levels = c('not_analyzed','analyzed')),
          species = 'baleen')
 
-
 # narw data: set up for plot
 narw_summary<-status_summary %>% 
-  transmute(Year, n_days = rec_days_year, analyzed = narw_days_year) %>% 
+  transmute(year, n_days = rec_days_baleen_year, analyzed = narw_days_year) %>% 
   mutate(not_analyzed = n_days - analyzed) %>% 
   select(-n_days) %>% 
-  pivot_longer(!Year, names_to = "effort", values_to = "count") %>% 
+  pivot_longer(!year, names_to = "effort", values_to = "count") %>% 
   mutate(effort = factor(effort, levels = c('not_analyzed','analyzed')),
          species = 'narw')
 
 # beaked whale data: set up for plot
 beaked_summary<-status_summary %>% 
-  transmute(Year, n_days = rec_days_beaked_year, analyzed = beaked_days_year) %>% 
+  transmute(year, n_days = rec_days_beaked_year, analyzed = beaked_days_year) %>% 
   mutate(not_analyzed = n_days - analyzed) %>% 
   select(-n_days) %>% 
-  pivot_longer(!Year, names_to = "effort", values_to = "count") %>% 
+  pivot_longer(!year, names_to = "effort", values_to = "count") %>% 
   mutate(effort = factor(effort, levels = c('not_analyzed','analyzed')),
          species = 'beaked')
 
 # put it all back together
 
 all_spp <- baleen_summary %>% 
-  #full_join(narw_summary) %>% 
+  full_join(narw_summary) %>% 
   full_join(beaked_summary) %>% 
-  mutate(species = factor(species, levels = c("baleen", "beaked")))
+  mutate(species = factor(species, levels = c('narw','baleen', 'beaked')))
 
-  
-# plot figure
+#--------------------------------------
+
+# Create figure
 
 theme_set(theme_bw())
 
-test<-ggplot() + 
+figure<-ggplot() + 
   
-  facet_wrap(~species, ncol = 1) +
+  facet_wrap(~species, ncol = 1,
+             labeller = as_labeller(c(`narw` = "North Atlantic right whales",
+                                      `baleen` = "Blue, fin, humpback, minke, & sei whales",
+                                      `beaked` = "Beaked whales"))) +
   
   geom_bar(data = all_spp, 
-           aes(y = count, x = Year, fill = effort),
+           aes(y = count, x = year, fill = effort),
            stat = 'identity',
            position = position_stack()) +
   
   ylab("Recording days") +
+  xlab("Year") +
   
-  scale_fill_manual('Analysis Effort', values = c("skyblue","skyblue4")) +
+  scale_fill_manual('Analysis Effort', 
+                    values = c("skyblue","skyblue4"),
+                    labels = c("pending", "complete")) +
 
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
+        strip.text = element_text(size = 11, face = 'bold'),
         text = element_text(size = 10))
   
-test  
+
+figurename <- paste0("PAM_analysis_summary_", Sys.Date(), ".png") 
+ggsave(figurename, figure, width = 6.5, height = 5, units = "in")
   
   
   
